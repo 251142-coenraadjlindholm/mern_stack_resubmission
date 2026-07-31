2cef4e92728ce2eb2e9adbf91f9fcf111d356cb0
251142-coenraadjlindholm <251142@virtualwindow.co.za>
2026-07-31 12:16:12 +0200

    Completed assignment according to the brief

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders task manager heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByRole('heading', { name: /manchester united task manager/i });
+  expect(headingElement).toBeInTheDocument();
 });
